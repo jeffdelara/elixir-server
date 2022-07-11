7b11c2aab171aa738174ede2066074a10209a19c
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-07-11 23:29:56 +0800

    params

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,11 +1,33 @@
-defmodule Servy.Parser do
-  def parse(request) do
-    [method, path, _] = 
-      request 
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
+# request = """
+# POST /bears HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+# Content-Type: application/x-www-form-urlencoded
+# Content-Length: 21
 
-    %{ method: method, status: nil, path: path, resp_body: "" }
+# name=Baloo&type=Brown
+# """
+
+defmodule Servy.Parser do
+  alias Servy.Conv
+
+  def parse(request) do
+
+    [top, params_string] = String.split(request, "\n\n")
+    [requests | headers] = String.split(top, "\n")
+    [method, path, _] = String.split(requests, " ")
+
+    params = parse_params(params_string)
+
+    %Conv{
+      method: method,
+      path: path,
+      params: params
+    }
+  end
+
+  def parse_params(params_string) do
+    params_string |> String.trim |> URI.decode_query
   end
 end
